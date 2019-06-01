a6bb68533a83e6069df5f5c6b27860fdcbe036f7
Theodore R. Smith <theodore@phpexperts.pro>
2019-06-01 10:11:37 -0500

    [m] Fixed the project link in the license headers.

--- examples/MyFuzzyDTO.php
+++ examples/MyFuzzyDTO.php
@@ -1,5 +1,17 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of SimpleDTO, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2019 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *  GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *  https://www.phpexperts.pro/
+ *  https://github.com/phpexpertsinc/SimpleDTO
+ *
+ * This file is licensed under the MIT License.
+ */
+
 namespace PHPExperts\SimpleDTO\Examples;
 
 require_once __DIR__ . '/../vendor/autoload.php';
