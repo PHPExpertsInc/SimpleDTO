07c11bdbc8db7560a58665929984cc1edb746398
Theodore R. Smith <theodore@phpexperts.pro>
2020-04-08 11:01:36 -0500

    Validate WriteOnce DTOs on toArray() and toSerialize().

--- tests/WriteOnceTest.php
+++ tests/WriteOnceTest.php
@@ -103,6 +103,7 @@ JSON;
 
         try {
             $writeOnceDTO->toArray();
+            self::fail('Did not invalidate an invalid WriteOnce DTO when toArray() was called.');
         } catch (InvalidDataTypeException $e) {
             $this->assertEquals($expected, $e->getReasons());
         }
