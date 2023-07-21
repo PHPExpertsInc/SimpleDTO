e58829f61722c509e8cd1c74924cdeb103020681
Theodore R. Smith <theodore@phpexperts.pro>
2023-07-20 20:07:02 -0500

    Upgraded to PHPUnit v10 and created a system for supporting individual phpunit.xml for each PHPUnit version.

--- test.sh
+++ test.sh
@@ -2,11 +2,20 @@
 
 time for PHPV in 7.2 7.3 7.4 8.0 8.1 8.2; do
     PHP_VERSION=$PHPV composer update
+    PHPUNIT_V=''
+    if [ $PHPV == '7.2' ]; then
+        PHPUNIT_V='8'
+    elif [ $PHPV == '7.3' ] || [ $PHPV == '7.4' ] || [ $PHPV == '8.0' ]; then
+        PHPUNIT_V='9'
+    else
+        PHPUNIT_V='10'
+    fi
+
 
     if [ $PHPV == '8.2' ]; then
-        PHP_VERSION=$PHPV-debug phpunit
+        PHP_VERSION=$PHPV-debug phpunit -c phpunit.v${PHPUNIT_V}.xml
     else
-        PHP_VERSION=$PHPV phpunit
+        PHP_VERSION=$PHPV phpunit -c phpunit.v${PHPUNIT_V}.xml
     fi
 
     echo "Tested: PHP v$PHPV"
