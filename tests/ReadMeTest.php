f1c23fa77b4ceb58247360ccba3060284b4d07b3
Theodore R. Smith <theodore@phpexperts.pro>
2025-03-13 17:40:35 -0500

    Version 3.8.0

--- tests/ReadMeTest.php
+++ tests/ReadMeTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of SimpleDTO, a PHP Experts, Inc., Project.
  *
- * Copyright © 2024 PHP Experts, Inc.
+ * Copyright © 2024-2025 PHP Experts, Inc.
  * Author: Theodore R. Smith <theodore@phpexperts.pro>
  *   GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
  *   https://www.phpexperts.pro/
