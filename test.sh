fe2e8af7d0a2f593bc7e558527b262dd929f1e7c
Theodore R. Smith <theodore@phpexperts.pro>
2025-03-13 16:36:45 -0500

    Upgraded to phpexperts/dockerize v12.1.0.

--- test.sh
+++ test.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-time for PHPV in 7.4 8.0 8.1 8.2 8.3 8.4; do
-    PHP_VERSION=$PHPV composer update
-    PHPUNIT_V=''
-    if [ $PHPV == '7.2' ]; then
-        PHPUNIT_V='8'
-    elif [ $PHPV == '7.3' ] || [ $PHPV == '7.4' ] || [ $PHPV == '8.0' ]; then
-        PHPUNIT_V='9'
-    elif [ $PHPV == '8.1' ]; then
-        PHPUNIT_V='10'
-    else
-        PHPUNIT_V='11'
-    fi
-
-
-    if [ $PHPV == '8.3' ]; then
-        PHP_VERSION=$PHPV-debug phpunit -c phpunit.v${PHPUNIT_V}.xml
-    else
-        PHP_VERSION=$PHPV phpunit -c phpunit.v${PHPUNIT_V}.xml
-    fi
-
-    echo "Tested: PHP v$PHPV"
-    sleep 2
-done
-
+php-ci.sh
